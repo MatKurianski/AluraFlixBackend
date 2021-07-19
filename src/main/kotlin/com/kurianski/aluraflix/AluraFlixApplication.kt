fa798fc3cb95e27be5ff438be87fa40e2b721f82
Matheus Aquati Kurianski <matheus1340@hotmail.com>
2021-07-19 18:54:24 -0300

    Primeiro commit

---
+++ src/main/kotlin/com/kurianski/aluraflix/AluraFlixApplication.kt
@@ -0,0 +1,11 @@
+package com.kurianski.aluraflix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraFlixApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraFlixApplication>(*args)
+}
